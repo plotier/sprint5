06e004b51b2855ea65ebc930ba288d0ab1a51545
plotier <plotier15@gmail.com>
2021-12-26 19:26:27 +0100

    exercise 2 done

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-const chiste = () => { 
-    alert ("chiste")
+const jokeFetch = async () => {
+    const response = await fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    const joke = await response.json();
+    let chisteFinale = joke.joke;
+    const chisteTexto = document.getElementById("chisteTexto");
+    return chisteTexto.textContent = await chisteFinale;
+
 }
+
